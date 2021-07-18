ba096a0740a910fecae23e0864a7df96031bc8dc
Namish295 <81818479+Namish295@users.noreply.github.com>
2021-07-18 19:24:17 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,35 @@
+canvas=document.getElementById("olympicLogo");
+ctx=canvas.getContext("2d");
+color="blue";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(325, 210, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+color="black";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(425, 210, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+color="red";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(525, 210, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+color="yellow";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.arc(375, 250, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+color="green";
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(475, 250, 40, 0, 2*Math.PI);
+ctx.stroke();
